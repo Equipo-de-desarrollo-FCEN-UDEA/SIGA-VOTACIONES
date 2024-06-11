888041a99b258dbf057d13d10f92d52136b69e54
Esteban Durango <estebandurangov>
2024-06-11 14:43:07 -0500

    se construyen los modelos VotingType y Vote

--- app/models/Voting.py
+++ app/models/Voting.py
@@ -5,19 +5,20 @@ from cuid import cuid
 
 from sqlmodel import SQLModel, Field, Relationship
 
-from app.schemas.enums import StatusName
-from app.schemas.vote import Vote
+from app.models.enums import StatusName
+from app.models.Vote import Vote
 
 
 
 class VotingBase(SQLModel):
     status: StatusName
     
+    
 
 class Voting(VotingBase, table=True):
     id: str | None = Field(default=cuid(), primary_key=True)
 
-    votes: List[Vote] = Relationship(back_populates="voting")
+    votes: list[Vote] = Relationship(back_populates="voting")
 
     created_at: datetime = Field(default_factory=datetime.now, alias="createAt")
     updated_at: datetime = Field(default_factory=datetime.now, alias="updateAt")
