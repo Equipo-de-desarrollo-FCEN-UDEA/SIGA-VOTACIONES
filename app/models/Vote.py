c81e4cd10b50a0c2926164552f29ef059721aebf
Hdavid1123 <harversan1123@gmail.com>
2024-06-12 16:12:03 -0500

    Restructuración proyecto con base de datos de prueba en sqlite

--- app/models/Vote.py
+++ app/models/Vote.py
@@ -1,20 +1,21 @@
-# vote.py
 from typing import Optional
 from cuid import cuid
 from sqlmodel import Relationship, SQLModel, Field
 from datetime import datetime
 
 from app.models.enums import VoteType
+from app.models.Voting import Voting
+
 
 class VoteBase(SQLModel):
     vote_type: VoteType
     user_id: int
-    voting_id: str = Field(foreign_key="voting.id")
+    voting_id: str = Field(foreign_key="votings.id")
 
 class Vote(VoteBase):
     id: str = Field(default=cuid(), primary_key=True)
 
-    voting: "voting" = Relationship(back_populates="votes") # type: ignore
+    voting: Voting = Relationship(back_populates="votes")
 
     created_at: datetime = Field(default_factory=datetime.now, alias="createAt")
     updated_at: datetime = Field(default_factory=datetime.now, alias="updateAt")
