94626f7f9ff7c398efa8955dd4a89d37d29c9317
Esteban Durango <estebandurangov>
2024-06-13 14:23:53 -0500

    se crea la clase abstracta BaseService y se crean los servicios para todos los modelos con id como PK

--- app/api/router.py
+++ app/api/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.api.routes import ping
 from app.api.routes.v1.Vote import vote_router
+from app.api.routes.v1.Status import status_router
 
 api_router = APIRouter()
 
 api_router.include_router(ping.router, tags=["ping"])
 api_router.include_router(vote_router, prefix="/vote", tags=["vote"])
+api_router.include_router(status_router, prefix="/status", tags=["status"])
